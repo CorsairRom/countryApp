842cb6298fdfb0cdc2366d30436d08f3ee326c23
CorsairRom <ric.romero@duocuc.cl>
2023-06-23 00:36:10 -0400

    firstComit

---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [
+  ]
+})
+export class ByCountryPageComponent {
+  public countries: Country[] = [];
+
+
+  constructor(private countriesServices: CountriesService)   {
+
+  }
+
+  searchByCountry(term: string):void {
+    if (term.length === 0) return;
+
+    this.countriesServices.searchCountry(term).subscribe(countries => {
+      this.countries = countries;
+    });
+
+  }
+}
